d993a504c1fb7c952c7cc17d49004106d710e62d
general-obi-one-kenobi <aadi.aiyer@gmail.com>
2026-09-16 03:20:24 +0000

    oop design and code activity

--- CrashCourse/Scammer.java
+++ CrashCourse/Scammer.java
@@ -18,6 +18,54 @@ public class Scammer {
         this.ethnicity = ethnicity;
         this.gender = gender;
 
+        health = 100;
+        money = 100.0;
+        wantedLevel = 0;
+        inJail = false;
+        carType = basic;
+        businessPartners = 0;
+        houseLevel = 1;
+
+    }
+
+    public void scamCall() {
+        money += 25.0;
+        wantedLevel += 1;
+        if (wantedLevel >= 5) {
+            inJail = true;
+            System.out.println("You got arrested.");
+    }
+
+    public void makeDeal() {
+        businessPartners += 1;
+        money += 50.0;
+    }
+    
+    public void sellValuables() {
+        money += 100.0;
+    }
+
+    public void evadeTaxes() {
+        wantedLevel += 1;
+        if (wantedLevel >= 5) {
+            inJail = true;
+            System.out.println("You got arrested.");
+        }
+    }
+
+        public void takeDamage(int amount) {
+        health -= amount;
+        if (health < 0) {
+            System.out.println("You died.");
+        }
+    }
+
+    public void attackPlayer() {
+        wantedLevel += 1;
+            if (wantedLevel >= 5) {
+                inJail = true;
+                System.out.println("You got arrested.");
+
     }
     
 }
